7f1f2245545c40170b40d1e06735f93c27923251
soebon <htskawasoe1357@gmail.com>
2023-03-30 16:23:31 +0900

    デプロイ用にコミット

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <HeaderComponent />
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <FooterComponent />
+  </v-app>
+</template>
+
+<script>
+import FooterComponent from '../components/FooterComponent.vue'
+import HeaderComponent from '../components/HeaderComponent.vue'
+
+export default {
+  name: 'DefaultLayout',
+  components: { FooterComponent, HeaderComponent },
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+    }
+  },
+}
+</script>
